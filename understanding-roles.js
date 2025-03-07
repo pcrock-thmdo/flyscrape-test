da1c168a4fa7ac122426e88b80fa3f012771da68
Philip Crockett <philip.crockett@thermondo.de>
2025-03-07 17:21:34 +0100

    handle beta roles

--- understanding-roles.js
+++ understanding-roles.js
@@ -9,11 +9,13 @@ export default function ({ doc }) {
     const roles = table.find("tr")
         .map(row => {
             return {
-                title: row.find("td.role-description > .role-title").text().trim(),
+                title: row.find("td.role-description > .role-title").text().trim()
+                    .replace(/\nBeta$/g, ""),  // delete "Beta" at end. is_beta below will handle that for us
                 id: row.find("td.role-description > .iamperm-marginless").text().trim()
                     .replace(/^\(|\)$/g, ""),  // delete parens at begin and end of text
                 description: row.find("td.role-description > span.role-description > p").text().trim(),
-                permissions: row.find("td.role-permissions code").map(x => x.text())
+                permissions: row.find("td.role-permissions code").map(x => x.text()),
+                is_beta: row.find("td.role-description .launch-stage-pre-ga").text().trim() != ""
             };
         })
         .filter(row => row.description.length > 0 && row.permissions.length > 0);
